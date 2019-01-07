6dca172f4fcc7b2d7ceeb149c4e0e45acc7f8f60
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-07 12:27:59 -0800

    results picture and reordering of code

--- Calcualtor/Calcualtor/Program.cs
+++ Calcualtor/Calcualtor/Program.cs
@@ -13,44 +13,52 @@ namespace Calcualtor
             int numOne;
             int numTwo;
             string operation;
-            int answer;
+            int result;
 
             /*takes in users first number*/
             Console.WriteLine("Enter your first number");
             numOne = Convert.ToInt32(Console.ReadLine());
 
+            /* specify the type of operation */
+            Console.WriteLine("Enter operation type ( + , - , * , / ) ");
+            operation = Console.ReadLine();
+
             /*takes in users second number*/
             Console.WriteLine("Enter your second number");
             numTwo = Convert.ToInt32(Console.ReadLine());
 
+           
 
-            Console.WriteLine("Enter operation type ( + , - , * , / ) ");
-            operation = Console.ReadLine();
-
+             //add nums 
             if (operation == "+")
             {
-                answer = numOne + numTwo;
-                Console.WriteLine( numOne + " + " +  numTwo + " = " + answer);
+                result = numOne + numTwo;
+                Console.WriteLine( numOne + " + " +  numTwo + " = " + result);
 
             }
+
+            //subtracts num 
             else if (operation == "-")
             {
-                answer = numOne - numTwo;
-                Console.WriteLine( numOne + " - " + numTwo + " = " + answer);
+                result = numOne - numTwo;
+                Console.WriteLine( numOne + " - " + numTwo + " = " + result);
 
             }
+            //multiply nums
             else if (operation == "*")
             {
-                answer = numOne * numTwo;
-                Console.WriteLine(numOne + " * " + numTwo + " = " + answer);
+                result = numOne * numTwo;
+                Console.WriteLine(numOne + " * " + numTwo + " = " + result);
 
             }
+            //divide nums
             else if (operation == "/")
             {
-                answer = numOne / numTwo;
-                Console.WriteLine(numOne + " / " + numTwo + " = " + answer);
+                result = numOne / numTwo;
+                Console.WriteLine(numOne + " / " + numTwo + " = " + result);
 
             }
+            //result of improper operation
             else
             {
                 Console.WriteLine("That is the incorrect format. Try again");
