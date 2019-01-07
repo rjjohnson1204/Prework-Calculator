8a6433d12f8cc570023a6e0825fe8eb7ce675365
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-07 11:50:29 -0800

    made calculator

---
+++ Calcualtor/Calcualtor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcualtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne;
+            int numTwo;
+            string operation;
+            int answer;
+
+            /*takes in users first number*/
+            Console.WriteLine("Enter your first number");
+            numOne = Convert.ToInt32(Console.ReadLine());
+
+            /*takes in users second number*/
+            Console.WriteLine("Enter your second number");
+            numTwo = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter operation type ( + , - , * , / ) ");
+            operation = Console.ReadLine();
+
+            if (operation == "+")
+            {
+                answer = numOne + numTwo;
+                Console.WriteLine( numOne + " + " +  numTwo + " = " + answer);
+
+            }
+            else if (operation == "-")
+            {
+                answer = numOne - numTwo;
+                Console.WriteLine( numOne + " - " + numTwo + " = " + answer);
+
+            }
+            else if (operation == "*")
+            {
+                answer = numOne * numTwo;
+                Console.WriteLine(numOne + " * " + numTwo + " = " + answer);
+
+            }
+            else if (operation == "/")
+            {
+                answer = numOne / numTwo;
+                Console.WriteLine(numOne + " / " + numTwo + " = " + answer);
+
+            }
+            else
+            {
+                Console.WriteLine("That is the incorrect format. Try again");
+                Console.ReadLine();
+            }
+;        }
+    }
+}
